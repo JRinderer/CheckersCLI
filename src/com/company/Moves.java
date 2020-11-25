f7bee32c2852248026c344e07f664637c5f8dc07
John RInderer <rindy555@gmail.com>
2020-11-24 20:39:54 -0600

    Modified moves and removed unnecessary getters and setters

--- src/com/company/Moves.java
+++ src/com/company/Moves.java
@@ -9,56 +9,6 @@ public class Moves {
     Piece piece;
     Player player;
 
-    public Player getPlayer() {
-        return player;
-    }
-
-    public void setPlayer(Player player) {
-        this.player = player;
-    }
-
-    public int getStartX() {
-        return startX;
-    }
-
-    public void setStartX(int startX) {
-        this.startX = startX;
-    }
-
-    public int getStartY() {
-        return startY;
-    }
-
-    public void setStartY(int startY) {
-        this.startY = startY;
-    }
-
-    public int getEndX() {
-        return endX;
-    }
-
-    public void setEndX(int endX) {
-        this.endX = endX;
-    }
-
-    public int getEndY() {
-        return endY;
-    }
-
-    public void setEndY(int endY) {
-        this.endY = endY;
-    }
-
-    public Piece getPiece() {
-
-        return piece;
-    }
-
-    public void setPiece(Piece piece) {
-
-        this.piece = piece;
-    }
-
     //test
     public boolean validMove(Board board, Piece piece) {
         //regular pieces can only move forward DO NOT ALLOW ABS on regular pieces
